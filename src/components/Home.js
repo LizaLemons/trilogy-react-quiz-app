49fe4e9b5641acd4375010e6ed826e4eb7578d41
Liza Ramo <liza.h.ramo@gmail.com>
2018-08-27 08:37:15 -0500

    added https to google fonts url

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
   render(){
     return(
       <div>
-        Click below to begin the quiz.
+        Click below to begin the quiz!
         <br />
         <button><Link onClick={this.props.nextQuestionFxn} to={"/question/1"}>Begin</Link></button>
       </div>
